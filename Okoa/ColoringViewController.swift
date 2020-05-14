bcd5a228ad77c8c5756a105b061dc56888434e4e
evanrenanto <62125175+evanrenanto@users.noreply.github.com>
2020-05-14 14:54:16 +0700

    errornya kelar tapi masih halu

--- Okoa/ColoringViewController.swift
+++ Okoa/ColoringViewController.swift
@@ -20,6 +20,7 @@ class ColoringViewController: UIViewController {
     var vehicleSketch: [UIImage] = [#imageLiteral(resourceName: "Bis"), #imageLiteral(resourceName: "Mobil"), #imageLiteral(resourceName: "Pesawat"), #imageLiteral(resourceName: "Sepeda")]
     
     var items: [UIColor] = [#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0, green: 0.04544427991, blue: 1, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0, green: 0.6007654667, blue: 0, alpha: 1), #colorLiteral(red: 0.3371880651, green: 1, blue: 0.4706221223, alpha: 1), #colorLiteral(red: 1, green: 0.6243972182, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 0.9899911284, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)]
+    var cobaCOlor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,14 +63,15 @@ extension ColoringViewController: UICollectionViewDelegate, UICollectionViewData
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath )
-        if let view = cell.viewWithTag(1){
-            view.backgroundColor = items[indexPath.row]
-            view.layer.cornerRadius = 20
-        }
+        //if let view = cell.viewWithTag(1){
+        cell.contentView.backgroundColor = items[indexPath.row]
+        cell.layer.cornerRadius = 20
+        //}
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        canvasView.strokeColor = items[indexPath.row]
+        //canvasView.strokeColor = items[indexPath.row]
+        self.canvasView.strokeColor = items[indexPath.row]
     }
     
     
